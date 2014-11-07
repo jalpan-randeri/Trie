e0aa84f7cfd6922b8f07123370a32a27a6966e4d
Jalpan Randeri <jalpan@ccs.neu.edu>
2014-11-07 02:04:41 -0500

    added node defination

--- src/Node.java
+++ src/Node.java
@@ -1,5 +1,28 @@
+import java.util.HashMap;
+
 /**
- * Created by jalpanranderi on 11/7/14.
+ * Node is internal node inside the trie data structure
+ *
+ * Node
+ *  - char c
+ *  - boolean isTerminal
+ *  - HashMap<Character, Node> children
+ *
+ *
+ * Created by jalpan randeri on 11/7/14.
  */
 public class Node {
+    private char c;
+    private boolean isTerminal = false;
+    HashMap<Character, Node> children = new HashMap<Character, Node>();
+
+    public Node(){
+
+    }
+
+    public Node(char c) {
+        this.c = c;
+    }
+
+
 }
