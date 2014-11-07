7d96ba8623225fdaa5a8b4799722ab92c5c4600b
Jalpan Randeri <jalpan@ccs.neu.edu>
2014-11-07 02:05:03 -0500

    added insert method

--- src/Trie.java
+++ src/Trie.java
@@ -1,5 +1,23 @@
 /**
+ *
+ * Trie
+ *   - root
+ *
  * Created by jalpanranderi on 11/7/14.
  */
 public class Trie {
+    private static Node root = new Node();
+
+    public void insert(String word){
+        Node next = root;
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            // check if character is present inside the children
+            //  false than add it inside the list
+            if(!root.children.containsKey(c)){
+                root.children.put(c, new Node(c));
+            }
+            next = root.children.get(c);
+        }
+    }
 }
